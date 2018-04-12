f5cdbbc4ed5e0f23245823ff52696534e713f7e6
Danni <dannielleorlando@gmail.com>
2018-04-12 14:26:35 -0400

    adjusted config files

---
+++ server/db/index.js
@@ -0,0 +1,15 @@
+/**
+ * todo: connect to a postgres database
+ */
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('postgres://localhost:5432/offeredDB_dev')
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
