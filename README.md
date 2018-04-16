ce2977156282f7571f2ffa133f6d41c26d0be9ab
Danni <dannielleorlando@gmail.com>
2018-04-16 12:44:57 -0400

    set up database connection

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # offeredDB
+WIP database for the best tech companies in NYC
+Find the web crawler here: [tinySpider](https://github.com/mathildapurr/tinySpider)
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const app = express();
+module.exports = app;
+
+// logging middleware
+app.use(morgan('dev'));
+
+// parsing middleware
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// api routes
+//app.use('/api', require('./api'));
+
+// load static resources from public directory
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// error logging
+app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal Server Error.');
+});
+
+// start server
+app.listen(3000, () => console.log('server is eagerly listening on port 3000! '));
